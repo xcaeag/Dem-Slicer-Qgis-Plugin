c5174900cb98fb2bda5508def5efc601de062a89
culos <xavier.culos@eau-adour-garonne.fr>
2022-05-09 09:52:29 +0200

    Traduction

--- dem_slicer/i18n/fr.ts
+++ dem_slicer/i18n/fr.ts
@@ -3,88 +3,145 @@
 <context>
     <name>DemSlicer</name>
     <message>
+        <location filename="../dem_slicer.py" line="230"/>
+        <source>&amp;D.E.M. Slicer</source>
+        <translation>&amp;D.E.M. Slicer</translation>
+    </message>
+    <message>
+        <location filename="../dem_slicer.py" line="202"/>
+        <source>D.E.M. slicer</source>
+        <translation>D.E.M. slicer</translation>
+    </message>
+</context>
+<context>
+    <name>DemSlicerDockWidget</name>
+    <message>
+        <location filename="../dem_slicer_dockwidget.py" line="1064"/>
+        <source>Save parameters</source>
+        <translation>Sauver les paramètres</translation>
+    </message>
+    <message>
+        <location filename="../dem_slicer_dockwidget.py" line="1096"/>
+        <source>Ini files (*.ini)</source>
+        <translation>Fichiers Ini (*.ini)</translation>
+    </message>
+    <message>
+        <location filename="../dem_slicer_dockwidget.py" line="1096"/>
+        <source>Load parameters</source>
+        <translation>Charger les paramètres</translation>
+    </message>
+</context>
+<context>
+    <name>DemSlicerDockWidgetBase</name>
+    <message>
+        <location filename="../ui/dem_slicer_dockwidget_base.ui" line="14"/>
+        <source>D.E.M. Slicer</source>
+        <translation>D.E.M. Slicer</translation>
+    </message>
+    <message>
+        <location filename="../ui/dem_slicer_dockwidget_base.ui" line="28"/>
+        <source>render lines</source>
+        <translation>produire les lignes</translation>
+    </message>
+    <message>
+        <location filename="../ui/dem_slicer_dockwidget_base.ui" line="41"/>
+        <source>render polygons</source>
+        <translation>produire les polygones</translation>
+    </message>
+    <message>
+        <location filename="../ui/dem_slicer_dockwidget_base.ui" line="54"/>
+        <source>render ridges</source>
+        <translation>produire les lignes de crête</translation>
+    </message>
+    <message>
+        <location filename="../ui/dem_slicer_dockwidget_base.ui" line="88"/>
+        <source>Save parameters</source>
+        <translation>Sauver les paramètres</translation>
+    </message>
+    <message>
+        <location filename="../ui/dem_slicer_dockwidget_base.ui" line="91"/>
+        <source>S</source>
+        <translation>S</translation>
+    </message>
+    <message>
+        <location filename="../ui/dem_slicer_dockwidget_base.ui" line="101"/>
+        <source>Load parameters</source>
+        <translation>Charger les paramètres</translation>
+    </message>
+    <message>
+        <location filename="../ui/dem_slicer_dockwidget_base.ui" line="104"/>
+        <source>L</source>
+        <translation>C</translation>
+    </message>
+    <message>
+        <location filename="../ui/dem_slicer_dockwidget_base.ui" line="113"/>
+        <source>Z shift</source>
+        <translation>Décalage des profils (en hauteur)</translation>
+    </message>
+    <message>
+        <location filename="../ui/dem_slicer_dockwidget_base.ui" line="126"/>
+        <source>Obs :   +</source>
+        <translation>Obs :   +</translation>
+    </message>
+    <message>
+        <location filename="../ui/dem_slicer_dockwidget_base.ui" line="155"/>
+        <source>Reset zone</source>
+        <translation>Réinitialiser la vue</translation>
+    </message>
+    <message>
+        <location filename="../ui/dem_slicer_dockwidget_base.ui" line="158"/>
+        <source>r</source>
+        <translation>r</translation>
+    </message>
+    <message>
+        <location filename="../ui/dem_slicer_dockwidget_base.ui" line="218"/>
+        <source>Ornamentation layer</source>
+        <translation>couche d&apos;habillage</translation>
+    </message>
+    <message>
+        <location filename="../ui/dem_slicer_dockwidget_base.ui" line="228"/>
         <source>DEM layer</source>
         <translation>Couche MNT</translation>
     </message>
     <message>
+        <location filename="../ui/dem_slicer_dockwidget_base.ui" line="243"/>
+        <source>xxxxxxxxxxxxxx</source>
+        <translation>xxxxxxxxxxxxxx</translation>
+    </message>
+    <message>
+        <location filename="../ui/dem_slicer_dockwidget_base.ui" line="268"/>
         <source>Build</source>
         <translation>Générer</translation>
     </message>
-
     <message>
-        <source>Start</source>
-        <translation>Démarrer</translation>
-    </message>
-
-    <message>
-        <source>Line count</source>
-        <translation>Nombre de profils</translation>
-    </message>
-
-    <message>
+        <location filename="../ui/dem_slicer_dockwidget_base.ui" line="275"/>
         <source>Z exageration</source>
-        <translation>Accentuer le relief</translation>
+        <translation>Accentuation du relief</translation>
     </message>
-
     <message>
-        <source>X Step</source>
-        <translation>Ecart entre deux mesures d'altitude</translation>
-    </message>
-
-    <message>
-        <source>Z shift</source>
-        <translation>Décalage des profils (en hauteur)</translation>
-    </message>
-
-    <message>
-        <source>render lines</source>
-        <translation>produire les lignes</translation>
-    </message>
-
-    <message>
-        <source>base height</source>
-        <translation>Hauteur de la base</translation>
-    </message>
-    
-    <message>
-        <source>render polygons</source>
-        <translation>produire les polygones</translation>
-    </message>
-
-    <message>
-        <source>render ridges</source>
-        <translation>produire les lignes de crête</translation>
-    </message>
-
-    <message>
+        <location filename="../ui/dem_slicer_dockwidget_base.ui" line="288"/>
         <source>Orthogonal view</source>
         <translation>Vue orthogonale</translation>
     </message>
-
     <message>
-        <source>Ornamentation layer</source>
-        <translation>couche d'habillage</translation>
+        <location filename="../ui/dem_slicer_dockwidget_base.ui" line="317"/>
+        <source>Start</source>
+        <translation>Démarrer</translation>
     </message>
-
     <message>
-        <source>Save parameters</source>
-        <translation>Sauver les paramètres</translation>
+        <location filename="../ui/dem_slicer_dockwidget_base.ui" line="324"/>
+        <source>Line count</source>
+        <translation>Nombre de profils</translation>
     </message>
-
     <message>
-        <source>L</source>
-        <translation>C</translation>
+        <location filename="../ui/dem_slicer_dockwidget_base.ui" line="334"/>
+        <source>X Step</source>
+        <translation>Ecart entre deux mesures d&apos;altitude</translation>
     </message>
-
     <message>
-        <source>Load parameters</source>
-        <translation>Charger les paramètres</translation>
+        <location filename="../ui/dem_slicer_dockwidget_base.ui" line="363"/>
+        <source>base height</source>
+        <translation>Hauteur de la base</translation>
     </message>
-
-    <message>
-        <source>Reset zone</source>
-        <translation>Réinitialiser la vue</translation>
-    </message>
-
-    </context>
+</context>
 </TS>
---
+++ dem_slicer/plugin_translation.pro
@@ -0,0 +1,6 @@
+SOURCES         = dem_slicer.py \
+                  dem_slicer_dockwidget.py
+
+FORMS           = ui/dem_slicer_dockwidget_base.ui
+
+TRANSLATIONS    = i18n/fr.ts
